47ec2c9f32bb81aea14abdd62d64eb9abe119d58
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-15 18:44:03 +0200

    Initialize shared localization

--- visualization/src/main.tsx
+++ visualization/src/main.tsx
@@ -1,15 +1,22 @@
+import { LocalizationProvider } from "@moritzbrantner/i18n/react";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
+import { createRaidDefenseI18n } from "./localization";
 import RootApp from "./RootApp";
+import { supportedLocales } from "./translations";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Missing #root mount point");
 }
 
+const { i18n } = await createRaidDefenseI18n();
+
 createRoot(rootElement).render(
   <React.StrictMode>
-    <RootApp />
+    <LocalizationProvider fallbackLocale="en" i18n={i18n} supportedLocales={supportedLocales}>
+      <RootApp />
+    </LocalizationProvider>
   </React.StrictMode>,
 );
